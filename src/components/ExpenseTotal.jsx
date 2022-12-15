f91e34925dda6e88333be62dc64e63c1f253b2b2
rivalcoba <ivan.rivalcoba@gmail.com>
2022-12-14 22:04:16 -0600

    🌟 Expense Calculation

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 function ExpenseTotal() {
+  // Extraemos la varibale expenses del estado global
+  const { expenses } = useContext(AppContext);
+  // Calculando el total de lo gastado
+  const totalExpenses = expenses.reduce((prev, item) => {
+    let total = prev;
+    (total += item.cost);
+    return total;
+  }, 0);
   return (
     <div className="alert alert-primary">
-      <span>Gastos: $3000.00</span>
+      <span>
+        Gastos: $
+        {totalExpenses}
+      </span>
     </div>
   );
 }
