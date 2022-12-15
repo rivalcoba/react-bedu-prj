069b52049e8ac84fc41a8226f442af29f73bdab5
rivalcoba <ivan.rivalcoba@gmail.com>
2022-12-14 22:20:08 -0600

    🌟 Calc Remaining

--- src/components/Remaining.jsx
+++ src/components/Remaining.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 function Reamaining() {
+  // Obteniendo estado
+  const { expenses, budget } = useContext(AppContext);
+  const totalExpenses = expenses.reduce((prev, current) => {
+    let total = prev;
+    const expense = current;
+    total += expense.cost;
+    return total;
+  }, 0);
+  const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
   return (
-    <div className="alert alert-success">
-      <span>Remanente: $2000.00</span>
+    <div className={`alert ${alertType}`}>
+      <span>
+        Remanente: $
+        { budget - totalExpenses }
+      </span>
     </div>
   );
 }
